20d22c95a49edd60edf36d7709b8083e2113f52b
Muni Besen <munibesen@gmail.com>
2024-09-29 05:34:07 -0300

    better error and element extract

--- web_actions.py
+++ web_actions.py
@@ -1,20 +1,48 @@
+import atexit
+import traceback
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException
 from selenium.webdriver.remote.webelement import WebElement
 from enum import Enum
-import traceback
 from typing import List, Optional, Any, Dict
+import re
 import time
-import atexit
 
 class SelectorType(Enum):
     """Enum for selector types."""
     XPATH = 'xpath'
     CSS = 'css'
+
+def clean_traceback(tb: str) -> str:
+    """
+    Clean the traceback by removing unhelpful parts.
+    
+    Args:
+        tb (str): The original traceback string.
+    
+    Returns:
+        str: The cleaned traceback string.
+    """
+    lines = tb.splitlines()
+    cleaned_lines = []
+    stacktrace_found = False
+
+    for line in lines:
+        if stacktrace_found:
+            if re.match(r"#\d+\s0x[0-9a-fA-F]+", line.strip()):
+                continue
+            else:
+                stacktrace_found = False  # Stop skipping lines once we encounter a non-matching line
+        if "Stacktrace:" in line:
+            stacktrace_found = True
+            continue  # Skip the "Stacktrace:" line itself
+        cleaned_lines.append(line)
+
+    return "\n".join(cleaned_lines)
 
 class WebSession:
     def __init__(self, options: Optional[Dict[str, Any]] = None) -> None:
@@ -35,19 +63,34 @@ class WebSession:
         self.driver = webdriver.Chrome(options=chrome_options)
         atexit.register(self.close)
 
-
-    def go_to(self, url: str) -> bool:
+    def close(self) -> bool:
         """
-        Navigate to a URL.
-        
-        Args:
-            url (str): The URL to navigate to.
+        Close the browser session.
         
         Returns:
-            bool: True if navigation is successful.
+            bool: True if the session is successfully closed.
         """
-        self.driver.get(url)
+        if self.driver:
+            self.driver.quit()
         return True
+
+    def debug(self) -> None:
+        """
+        Open the browser and wait indefinitely for debugging purposes.
+        To stop this you can use CTRL+C.
+
+        Note:
+            This method will keep the browser open indefinitely.
+        """
+        print("Debug mode: Browser is open and waiting indefinitely.")
+        try:
+            stopped = True
+            while stopped:
+                time.sleep(10)
+        except KeyboardInterrupt:
+            print("Debug mode: Exiting on keyboard interrupt.")
+        finally:
+            self.close()
 
     def wait_for_element(self, selector_type: SelectorType, selector: str, timeout: int = 10) -> Optional[WebElement]:
         """
@@ -68,9 +111,13 @@ class WebSession:
                 return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
             else:
                 raise ValueError(f"Unsupported selector type: {selector_type}")
-        except Exception as e:
-            print(f"Error waiting for element: {e}")
-            traceback.print_exc()
+        except TimeoutException:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
+            return None
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return None
 
     def wait_for_elements(self, selector_type: SelectorType, selector: str, timeout: int = 10) -> List[WebElement]:
@@ -92,9 +139,13 @@ class WebSession:
                 return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, selector)))
             else:
                 raise ValueError(f"Unsupported selector type: {selector_type}")
-        except Exception as e:
-            print(f"Error waiting for elements: {e}")
-            traceback.print_exc()
+        except TimeoutException:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
+            return []
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return []
 
     def find_element(self, selector_type: SelectorType, selector: str, skip_wait: bool = False, timeout: int = 10) -> Optional[WebElement]:
@@ -110,15 +161,20 @@ class WebSession:
         Returns:
             WebElement: The found element, or None if not found.
         """
-        if skip_wait:
-            if selector_type == SelectorType.XPATH:
-                return self.driver.find_element(By.XPATH, selector)
-            elif selector_type == SelectorType.CSS:
-                return self.driver.find_element(By.CSS_SELECTOR, selector)
+        try:
+            if skip_wait:
+                if selector_type == SelectorType.XPATH:
+                    return self.driver.find_element(By.XPATH, selector)
+                elif selector_type == SelectorType.CSS:
+                    return self.driver.find_element(By.CSS_SELECTOR, selector)
+                else:
+                    raise ValueError(f"Unsupported selector type: {selector_type}")
             else:
-                raise ValueError(f"Unsupported selector type: {selector_type}")
-        else:
-            return self.wait_for_element(selector_type, selector, timeout)
+                return self.wait_for_element(selector_type, selector, timeout)
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
+            return None
 
     def find_elements(self, selector_type: SelectorType, selector: str, skip_wait: bool = False, timeout: int = 10) -> List[WebElement]:
         """
@@ -133,15 +189,20 @@ class WebSession:
         Returns:
             list: A list of found elements, or an empty list if none are found.
         """
-        if skip_wait:
-            if selector_type == SelectorType.XPATH:
-                return self.driver.find_elements(By.XPATH, selector)
-            elif selector_type == SelectorType.CSS:
-                return self.driver.find_elements(By.CSS_SELECTOR, selector)
+        try:
+            if skip_wait:
+                if selector_type == SelectorType.XPATH:
+                    return self.driver.find_elements(By.XPATH, selector)
+                elif selector_type == SelectorType.CSS:
+                    return self.driver.find_elements(By.CSS_SELECTOR, selector)
+                else:
+                    raise ValueError(f"Unsupported selector type: {selector_type}")
             else:
-                raise ValueError(f"Unsupported selector type: {selector_type}")
-        else:
-            return self.wait_for_elements(selector_type, selector, timeout)
+                return self.wait_for_elements(selector_type, selector, timeout)
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
+            return []
 
     def click(self, selector_type: SelectorType, selector: str, skip_wait: bool = False, timeout: int = 10) -> bool:
         """
@@ -161,9 +222,9 @@ class WebSession:
             if element:
                 return self.safe_click(element)
             return False
-        except Exception as e:
-            print(f"Error clicking element: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return False
 
     def safe_click(self, element: WebElement) -> bool:
@@ -172,7 +233,6 @@ class WebSession:
         
         Args:
             element (WebElement): The element to click.
-            timeout (int): The maximum time to wait for the element to be clickable.
         
         Returns:
             bool: True if the click is successful, False otherwise.
@@ -183,9 +243,9 @@ class WebSession:
         try:
             element.click()
             return True
-        except ElementClickInterceptedException as e:
-            print(f"Element click intercepted: {e}")
-            traceback.print_exc()
+        except ElementClickInterceptedException:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             # Find the element that intercepted the click
             interceptor_element = self.driver.execute_script("""
                 var elem = arguments[0];
@@ -199,9 +259,9 @@ class WebSession:
                     print(f"Clicking interceptor element: {interceptor_element}")
                     interceptor_element.click()
                     return True
-                except Exception as e:
-                    print(f"Error clicking interceptor element: {e}")
-                    traceback.print_exc()
+                except Exception:
+                    error_traceback = clean_traceback(traceback.format_exc())
+                    print(error_traceback)
             return False
 
     def right_click(self, selector_type: SelectorType, selector: str, skip_wait: bool = False, timeout: int = 10) -> bool:
@@ -224,9 +284,9 @@ class WebSession:
                 actions.context_click(element).perform()
                 return True
             return False
-        except Exception as e:
-            print(f"Error right-clicking element: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return False
 
     def type_text(self, selector_type: SelectorType, selector: str, text: str, skip_wait: bool = False, timeout: int = 10, interactable_timeout: int = 10) -> bool:
@@ -252,9 +312,9 @@ class WebSession:
                 element.send_keys(text)
                 return True
             return False
-        except Exception as e:
-            print(f"Error typing in element: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return False
 
     def clear(self, selector_type: SelectorType, selector: str, skip_wait: bool = False, timeout: int = 10, interactable_timeout: int = 10) -> bool:
@@ -279,9 +339,9 @@ class WebSession:
                 element.clear()
                 return True
             return False
-        except Exception as e:
-            print(f"Error clearing element: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return False
 
     def hover(self, selector_type: SelectorType, selector: str, skip_wait: bool = False, timeout: int = 10) -> bool:
@@ -304,18 +364,20 @@ class WebSession:
                 actions.move_to_element(element).perform()
                 return True
             return False
-        except Exception as e:
-            print(f"Error hovering over element: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return False
 
-    def extract(self, selector_type: SelectorType, selector: str, attribute: Optional[str] = None, skip_wait: bool = False, timeout: int = 10) -> Optional[str]:
+
+    def extract(self, selector_type: Optional[SelectorType] = None, selector: Optional[str] = None, element: Optional[WebElement] = None, attribute: Optional[str] = None, skip_wait: bool = False, timeout: int = 10) -> Optional[str]:
         """
         Extract data from an element.
         
         Args:
             selector_type (SelectorType): The type of selector (XPATH or CSS).
             selector (str): The selector string.
+            element (WebElement): The WebElement object to extract data from.
             attribute (str): The attribute to extract. Use "__text__" to extract the text content.
             skip_wait (bool): Whether to skip waiting for the element.
             timeout (int): The maximum time to wait for the element.
@@ -324,7 +386,8 @@ class WebSession:
             str: The extracted data, or None if extraction fails.
         """
         try:
-            element = self.find_element(selector_type, selector, skip_wait, timeout)
+            if not element:
+                element = self.find_element(selector_type, selector, skip_wait, timeout)
             if element:
                 if attribute == "__text__":
                     return element.text
@@ -333,9 +396,9 @@ class WebSession:
                 else:
                     return element.text
             return None
-        except Exception as e:
-            print(f"Error extracting data: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return None
 
     def run_js(self, selector_type: SelectorType, selector: str, script: str, skip_wait: bool = False, timeout: int = 10) -> Optional[Any]:
@@ -359,9 +422,9 @@ class WebSession:
                 print(f"JavaScript execution result: {result}")
                 return result
             return None
-        except Exception as e:
-            print(f"Error running JavaScript: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return None
 
     def get_page_title(self) -> Optional[str]:
@@ -373,9 +436,9 @@ class WebSession:
         """
         try:
             return self.driver.title
-        except Exception as e:
-            print(f"Error getting page title: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return None
 
     def get_page_source(self) -> Optional[str]:
@@ -387,9 +450,9 @@ class WebSession:
         """
         try:
             return self.driver.page_source
-        except Exception as e:
-            print(f"Error getting page source: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return None
 
     def get_current_url(self) -> Optional[str]:
@@ -401,21 +464,10 @@ class WebSession:
         """
         try:
             return self.driver.current_url
-        except Exception as e:
-            print(f"Error getting current URL: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return None
-
-    def close(self) -> bool:
-        """
-        Close the browser session.
-        
-        Returns:
-            bool: True if the session is successfully closed.
-        """
-        if self.driver:
-            self.driver.quit()
-        return True
 
     def scroll(self, direction: str = "down", amount: Optional[int] = None, selector_type: Optional[SelectorType] = None, selector: Optional[str] = None, x: Optional[int] = None, y: Optional[int] = None, to_end: bool = False, timeout: int = 10) -> bool:
         """
@@ -468,21 +520,50 @@ class WebSession:
             else:
                 raise ValueError("Invalid scroll parameters")
             return True
-        except Exception as e:
-            print(f"Error scrolling: {e}")
-            traceback.print_exc()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
             return False
-        
 
-    def debug(self) -> None:
+    def go_to(self, url: str) -> None:
         """
-        Open the browser and wait indefinitely for debugging purposes.
+        Navigate to a URL.
+        
+        Args:
+            url (str): The URL to navigate to.
         """
-        print("Debug mode: Browser is open and waiting indefinitely.")
         try:
-            while True:
-                time.sleep(100000000)
-        except KeyboardInterrupt:
-            print("Debug mode: Exiting on keyboard interrupt.")
-        finally:
-            self.close()
+            self.driver.get(url)
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
+
+    def refresh(self) -> None:
+        """
+        Refresh the current page.
+        """
+        try:
+            self.driver.refresh()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
+
+    def back(self) -> None:
+        """
+        Navigate back in the browser history.
+        """
+        try:
+            self.driver.back()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
+
+    def forward(self) -> None:
+        """
+        Navigate forward in the browser history.
+        """
+        try:
+            self.driver.forward()
+        except Exception:
+            error_traceback = clean_traceback(traceback.format_exc())
+            print(error_traceback)
